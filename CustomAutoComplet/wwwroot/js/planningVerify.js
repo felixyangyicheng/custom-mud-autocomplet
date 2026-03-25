9f2390b9b30bf9b61b8710b47bf66f964a9a44c5
felixyangyicheng <felixyangyicheng@gmail.com>
2026-03-25 20:47:36 +0100

    virtualize horizontal scroll

---
+++ CustomAutoComplet/wwwroot/js/planningVerify.js
@@ -0,0 +1,7 @@
+﻿window.getElementRect = (element) => {
+    const r = element.getBoundingClientRect();
+    return { top: r.top, height: r.height };
+};
+
+window.getScrollLeft = (el) => el.scrollLeft;
+window.getClientWidth = (el) => el.clientWidth;
